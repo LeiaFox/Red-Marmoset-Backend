071f5b70ed1f807eb2983e731a9c975f23a67d09
LeiaFox <leonardo_c_martinez@hotmail.com>
2023-01-27 17:01:57 +0000

    SUPER MARIO

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const cors = require('cors');
+const jwt = require("jsonwebtoken")
 
 const app = express();
 const bodyParser = require("body-parser");
@@ -12,20 +13,22 @@ const testUsers = [
 
 app.use(cors({
   origin: '*', // replace with the address of your frontend
-  methods: ['POST', 'GET'],
+  methods: ['POST'],
   allowedHeaders: ['Content-Type']
 }));
 
 app.use(bodyParser.json());
 
-app.use('/login', (req, res) => {
+app.post('/login', (req, res) => {
   // check if user exists in the database
   // and if the password matches
   let user = testUsers.find(user => user.username === req.body.username && user.password === req.body.password);
   if (user) {
     // create and sign a JWT and send it back to the client
     const token = jwt.sign({user}, 'your_secret_key');
-    res.json({token});
+    res.json({
+      token
+    });
   } else {
     // send an error message
     res.status(401).json({error: 'Invalid login credentials'});

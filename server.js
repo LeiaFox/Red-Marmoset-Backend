75a8e58cb74a5ade87d1aaf39972d2a96f2623da
LeiaFox <leonardo_c_martinez@hotmail.com>
2023-01-27 11:59:44 +0000

    aaaaaaa

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-app.use(cors());
+const testUsers = [
+  {username: "testuser", email: "test@example.com", password: "password123"},
+  {username: "anotheruser", email: "another@example.com", password: "password456"},
+];
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
+app.post('/login', (req, res) => {
+  // check if user exists in the database
+  // and if the password matches
+  let user = testUsers.find(user => user.username === req.body.username && user.password === req.body.password);
+  if (user) {
+    // create and sign a JWT and send it back to the client
+    const token = jwt.sign({user}, 'your_secret_key');
+    res.json({token});
+  } else {
+    // send an error message
+    res.status(401).json({error: 'Invalid login credentials'});
+  }
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+app.listen(8080, () => {
+  console.log('Server started on http://localhost:8080');
+});

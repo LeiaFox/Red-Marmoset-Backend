abf88890345ed23c2760c8d82b65f3afbab53b57
LeiaFox <leonardo_c_martinez@hotmail.com>
2023-01-27 13:55:28 +0000

    fixing fixing

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
+
+const cors = require('cors');
+
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+const bodyParser = require("body-parser");
 
 const testUsers = [
   {username: "testuser", email: "test@example.com", password: "password123"},
   {username: "anotheruser", email: "another@example.com", password: "password456"},
 ];
 
-app.post('/login', (req, res) => {
+app.use(cors({
+  origin: '*', // replace with the address of your frontend
+  methods: ['POST', 'GET'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.use(bodyParser.json());
+
+app.use('/login', (req, res) => {
   // check if user exists in the database
   // and if the password matches
   let user = testUsers.find(user => user.username === req.body.username && user.password === req.body.password);
@@ -21,6 +31,8 @@ app.post('/login', (req, res) => {
     res.status(401).json({error: 'Invalid login credentials'});
   }
 });
+
+app.use(express.static('routes'));
 
 app.listen(8080, () => {
   console.log('Server started on http://localhost:8080');
